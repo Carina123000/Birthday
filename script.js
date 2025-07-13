09da6bfec8f4220dcfc41208c60a30d70f074284
Carina123000 <Sahudeepti564@gmail.com>
2025-07-14 00:23:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ to(
 '#frosting',
 {
   duration: 1,
-  morphSVG: '.cake__frosting--end' },
+  //morphSVG: '.cake__frosting--end' },
 
 0);
 
@@ -98,7 +98,7 @@ to('.candle__flame-outer', {
   duration: flickerSpeed,
   repeat: -1,
   yoyo: true,
-  morphSVG: '#flame-outer' }).
+  //morphSVG: '#flame-outer' }).
 
 to(
 '.candle__flame-inner',
@@ -106,7 +106,7 @@ to(
   duration: flickerSpeed,
   repeat: -1,
   yoyo: true,
-  morphSVG: '#flame-inner' },
+  //morphSVG: '#flame-inner' },
 
 0);
 
